07c27e4de1aff2696ac7704207e387c7ab68605a
“First <Kerger06@aol.com>
2020-09-30 14:38:25 -0500

    changed User component from  class to function

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,25 +1,16 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos  from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+const User = ({ user, loading, getUser, getUserRepos, repos, match  }) => {
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        // eslint-disable-next-line
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-
-    }
-
-    render() {
         const {
             name,
             avatar_url,
@@ -35,9 +26,7 @@ export class User extends Component {
             hireable,
             company
 
-        } = this.props.user
-
-        const { loading, repos } = this.props;
+        } = user
 
         
             if(loading) return <Spinner />;
@@ -96,8 +85,15 @@ export class User extends Component {
                 </div>
                 <Repos repos={repos} />
             </Fragment>
-        
-    }
+}
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+
 }
 
 export default User
